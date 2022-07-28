bcc852540092771ade01c6fb75d18e623bd0ef2f
tdyzzsp47 <tdyzzsp47@gmail.com>
2022-07-28 18:47:33 +0900

    backend の 関数を叩くためのApiClientを追加

---
+++ src/api/types.ts
@@ -0,0 +1,28 @@
+interface Mora {
+  text: string;
+  consonant: string;
+  consonant_length: number;
+  vowel: string;
+  vowel_length: number;
+  pitch: number;
+}
+
+interface AccentPhrase {
+  moras: Mora[];
+  accent: number;
+  pause_mora: Mora[]; 
+  is_interrogative: Boolean;
+}
+
+export interface VoiceQuery {
+  accent_phrases: AccentPhrase[];
+  speedScale: number;
+  pitchScale: number;
+  intonationScale: number;
+  volumeScale:number;
+  prePhonemeLength: number;
+  postPhonemeLength: number;
+  outputSamplingRate: number;
+  outputStereo: boolean;
+  kana: string;
+}
