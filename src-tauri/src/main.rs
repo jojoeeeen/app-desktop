6a261fec174ab5ae6b481d8ac92d71f03da851d8
tdyzzsp47 <tdyzzsp47@gmail.com>
2022-07-28 18:46:42 +0900

    backend に VoiceQuery を生成するための関数を追加

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,6 @@
 use tauri_plugin_store::PluginBuilder;
 use app::sample;
+use app::voice;
 
 #[cfg_attr(
   all(not(debug_assertions), target_os = "windows"),
@@ -9,7 +10,7 @@ use app::sample;
 fn main() {
   tauri::Builder::default()
     .plugin(PluginBuilder::default().build())
-    .invoke_handler(tauri::generate_handler![sample::sample_fn])
+    .invoke_handler(tauri::generate_handler![sample::sample_fn, voice::generate_query])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
